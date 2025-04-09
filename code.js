599aedda817d3b04683adfedbf8a27cfe067755d
megrose10 <157438545+megrose10@users.noreply.github.com>
2025-04-08 22:36:42 -0600

    Recursive Implementation

--- code.js
+++ code.js
@@ -1,3 +1,73 @@
 function tsp_hk(distance_matrix) {
-    return -1;
+    let numOfCities = distance_matrix.length;
+    let visited = [];
+    let visitedCities = [];
+    let shortestTour = Infinity;
+    let start = 0;
+
+    //Set every vertex in visited to false
+    for(let i = 0; i < numOfCities; i++) {
+        visited[i] = false;
+    }
+
+    //Set start vertex to true
+    visited[start] = true;
+
+    //Call heldKarpTSP to find best tour
+    shortestTour = heldKarpTSP(distance_matrix, visited, start, numOfCities);
+
+    return shortestTour;
+
 }
+
+function heldKarpTSP(cities, visited, currentCity, numOfCities) {
+    //If a city has been visited, add to visitedCities array
+    //for(let i = 0; i < visited.length; i++) {
+    //    if(visited[i]) {
+    //        visitedCities.push(i);
+    //    }
+    //}
+    let returnToStart = false;
+    //console.log("current city: " + currentCity);
+
+    //If there is a city that has not been visited, visitedCities will be false
+    //since we have not gone through all cities
+    let visitedCities = true;
+    for(let i = 0; i < numOfCities; i++) {
+        if(!visited[i]) {
+            visitedCities = false;
+            break;
+        }
+    }
+
+    //Once all cities have been visited, return to start
+    if(visitedCities == true) {
+        return cities[currentCity][0];
+    } 
+
+    //Minimum path set to infinity
+    let min = Infinity;
+
+    for(let nextCity = 0; nextCity < numOfCities; nextCity++) {
+        //If the city has not been visited
+        if(visited[nextCity]) {
+            continue;
+        }
+        //Will now be visited
+        visited[nextCity] = true;
+        //Find distance
+        //console.log(cities[currentCity][nextCity]);
+        let distanceWeight = cities[currentCity][nextCity] + heldKarpTSP(cities, visited, nextCity, numOfCities);
+        if(min > distanceWeight) {
+            min = distanceWeight;
+        }
+        visited[nextCity] = false;
+    }
+
+    return min;
+
+}
+
+//function tsp_hk(distance_matrix) {
+//    return -1;
+//}
