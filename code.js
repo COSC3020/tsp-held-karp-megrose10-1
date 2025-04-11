5a3481fb8ba61603d56a7cb40680db2bbec1853a
megrose10 <157438545+megrose10@users.noreply.github.com>
2025-04-10 21:22:58 -0600

    Still Working

--- code.js
+++ code.js
@@ -6,6 +6,7 @@ function tsp_hk(distance_matrix) {
     let start = 0;
     //Our memoization to keep track of distances we have calculated
     let savedDist = {};
+    let end = numOfCities - 1;
 
     //If there is one city, the distnace is 0
     if(numOfCities <= 1) {
@@ -38,13 +39,13 @@ function tsp_hk(distance_matrix) {
     visited[start] = true;
 
     //Call heldKarpTSP to find best tour
-    shortestTour = heldKarpTSP(distance_matrix, visited, start, numOfCities, savedDist);
+    shortestTour = heldKarpTSP(distance_matrix, visited, start, numOfCities, savedDist, end);
 
     return shortestTour;
 
 }
 
-function heldKarpTSP(cities, visited, currentCity, numOfCities, savedDist) {
+function heldKarpTSP(cities, visited, currentCity, numOfCities, savedDist, end) {
     //If a city has been visited, add to visitedCities array
     //for(let i = 0; i < visited.length; i++) {
     //    if(visited[i]) {
@@ -66,31 +67,48 @@ function heldKarpTSP(cities, visited, currentCity, numOfCities, savedDist) {
     //If there is a city that has not been visited, visitedCities will be false
     //since we have not gone through all cities
     let visitedCities = true;
+    let numOfUnvisitedCities = 0;
     for(let i = 0; i < numOfCities; i++) {
         if(!visited[i]) {
             visitedCities = false;
-            break;
+            numOfUnvisitedCities++;
+            //break;
         }
     }
 
-    //Once all cities have been visited, return to start
-    if(visitedCities == true) {
-        return cities[currentCity][0];
-    } 
+    if(numOfUnvisitedCities == 1){
+        //Once all cities have been visited, return to start
+        // if(visitedCities == true) {
+        //     return cities[currentCity][end];
+        // }
+        if(!visited[end]) {
+            return cities[currentCity][end];
+        }
+    }
+
+    //If all cities have been seen
+    if(numOfUnvisitedCities == 0) {
+        //If the city we are at is the end
+        if(currentCity == end) {
+            return 0;
+        }
+    }
+
+
 
     //Minimum path set to infinity
     let min = Infinity;
 
     for(let nextCity = 0; nextCity < numOfCities; nextCity++) {
         //If the city has not been visited
-        if(visited[nextCity]) {
+        if(visited[nextCity] || ((nextCity == end) && (numOfUnvisitedCities < (numOfCities - 1)))) {
             continue;
         }
         //Will now be visited
         visited[nextCity] = true;
         //Find distance
         //console.log(cities[currentCity][nextCity]);
-        let distanceWeight = cities[currentCity][nextCity] + heldKarpTSP(cities, visited, nextCity, numOfCities, savedDist);
+        let distanceWeight = cities[currentCity][nextCity] + heldKarpTSP(cities, visited, nextCity, numOfCities, savedDist, end);
         if(min > distanceWeight) {
             min = distanceWeight;
         }
